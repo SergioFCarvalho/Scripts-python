670278a499dcc67c6e4a637dea9000ba4255244a
Sérgio Carvalho <111389431+SergioFCarvalho@users.noreply.github.com>
2022-08-23 12:56:03 -0300

    add
    Add

---
+++ Desafio03.py
@@ -0,0 +1,5 @@
+print( '=== Desafio 3 ===')
+
+n1 = input( 'Primeiro número ')
+n2 = input ( 'segundo número ')
+print( 'A soma é   n1 + n2')
---
+++ desafio02.py
@@ -0,0 +1,7 @@
+print('====== Desafio 02 =======')
+
+dia= input(' Dia = ')
+mes= input(' Mes = ')
+ano= input(' Ano = ')
+print('Você nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + ' . Correto? ')
+
